59aed6a1c118bfa6921ded956eadfd94873264ca
Andy Wilson <info@andypi.co.uk>
2022-02-24 12:14:27 +0000

    initial commit

---
+++ mailbluster.py
@@ -0,0 +1,62 @@
+import requests
+import hashlib
+
+class Mailbluster():
+
+    def __init__(self, api_key):
+        '''
+        Sets the API key for the class instance
+        '''
+        self.api_key = api_key
+        self.base_url = 'https://api.mailbluster.com/api/'
+
+    def __perform_request(self, endpoint_url, typeofr, payload=None):
+        '''
+        Performs a generic API request and returns the JSON response
+        '''
+        headers = {"Authorization": self.api_key}
+        if typeofr == "get": r = requests.get(endpoint_url, headers=headers)
+        if typeofr == "post": r = requests.post(endpoint_url, data=payload, headers=headers)
+        if typeofr == "put": r = requests.put(endpoint_url, data=payload, headers=headers)
+        if typeofr == "delete": r = requests.delete(endpoint_url, data=payload, headers=headers)
+        return r.json()
+
+    def create_lead(self,email,first_name='',last_name=''):
+        '''
+        Creates a new lead *args and kwargs can we use here? unpack and add
+        '''
+        payload = {}
+        payload["firstName"] = first_name
+        payload["lastName"] = last_name
+        payload["email"] = email
+        payload["timezone"] = timezone
+        payload["ipAddress"] = ipAddress
+        payload["tags"] = tags
+        payload["meta"] = meta
+        payload["subscribed"] = 'true'
+        payload["overrideExisting"] = 'true'
+        return self.__perform_request(self.base_url + 'leads', typeofr="post", payload=payload)
+
+    def read_lead(self,lead_email):
+        '''
+        Reads a lead
+        '''
+        lead_md5_hash = (hashlib.md5(lead_email)).hexdigest()
+        return self.__perform_request(self.base_url + 'leads/' + lead_md5_hash, typeofr="get")
+
+    def update_lead(self,lead_email):
+        '''
+        Updates a lead ()
+        '''
+        addTags
+        removeTag
+        lead_md5_hash = (hashlib.md5(lead_email)).hexdigest()
+        return self.__perform_request(self.base_url + 'leads/' + lead_md5_hash, typeofr="put")
+
+    def delete_lead(self,lead_email):
+        '''
+        Deletes a lead
+        '''
+        lead_md5_hash = (hashlib.md5(lead_email)).hexdigest()
+        return self.__perform_request(self.base_url + 'leads/' + lead_md5_hash, typeofr="delete")
+
---
+++ newlead.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import re
+import os
+from mailbluster import Mailbluster
+
+mailbluster_api_key = os.environ['MAILBLUSTER_API_KEY']
+mailblusterclient = Mailbluster(mailbluster_api_key)
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    '''
+    Signup to mailbluster API
+    '''
+    if request.method == 'GET':
+        return render_template('index.html')
+    else: # POST METHOD
+        lead_email = re.sub('[^a-zA-Z0-9-_@.]', '', request.form['email'])
+        first_name = re.sub('[^a-zA-Z0-9-_ @.]', '', request.form['firstname'])
+        last_name = re.sub('[^a-zA-Z0-9-_@ .]', '', request.form['lastname'])
+        response = mailblusterclient.create_lead(lead_email,first_name,last_name)
+        if response['message'] =='Lead created':
+            status = 'You have been succesfully subscribed'
+        else:
+            status = 'An error occured'
+        return render_template('index.html', status=status)
+
+if __name__ == '__main__':
+    app.run()
